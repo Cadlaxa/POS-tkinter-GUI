e14d59ff9f81903bc7cf77cbd5f4d93f864c50e6
cadlaxa <cadag113003@gmail.com>
2024-05-20 15:16:14 +0800

    create initial login form with pseudo database hehe

---
+++ install modules.bat
@@ -0,0 +1,14 @@
+echo INSTALLATION WILL BEGIN
+
+:start
+cls
+
+cd \
+cd \python%python_ver%\Scripts\
+pip install tk
+pip install ruamel.yaml
+pip install pillow
+
+echo INSTALLATION COMPLETE!
+pause
+exit
