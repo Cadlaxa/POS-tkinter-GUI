551c0c26637713f538f1a4a6aa7aa435b373b0c4
cadlaxa <cadag113003@gmail.com>
2024-06-12 17:26:56 +0800

    update bat file for admin previleges

--- install modules.bat
+++ install modules.bat
@@ -1,6 +1,17 @@
 @echo off
-echo INSTALLATION WILL BEGIN
+:: Check for administrative privileges
+openfiles >nul 2>&1
+if '%errorlevel%' == '0' (goto :is_admin) else (goto :get_admin)
 
+:get_admin
+echo Installing with administrative privileges...
+cd /d "%~dp0"
+:: Request administrative privileges
+powershell -Command "Start-Process '%~f0' -Verb RunAs"
+exit /b
+
+:is_admin
+echo INSTALLATION WILL BEGIN
 :start
 cls
 
@@ -18,32 +29,30 @@ pip install ruamel.yaml
 pip install pillow
 pip install qrcode
 
-
 :: Define the source and intermediate directories
-set "source_font_dir=%cd%\POS-tkinter-GUI\Assets\Montserrat static fonts"
-set "intermediate_dir=%cd%\POS-tkinter-GUI\Assets\Montserrat static fonts"
-set "target_font_dir=C:\Windows\Fonts"
+set "intermediate_dir=%cd%POS-tkinter-GUI/Assets/Montserrat_static_fonts"
+set "target_font_dir=C:/Windows/Fonts"
 
 :: Install TTF fonts
 echo Installing fonts from "%intermediate_dir%" to "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-Black.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-BlackItalic.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-Bold.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-BoldItalic.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-ExtraBold.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-ExtraBoldItalic.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-ExtraLight.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-ExtraLightItalic.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-Italic.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-Light.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-LightItalic.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-Medium.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-MediumItalic.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-Regular.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-SemiBold.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-SemiBoldItalic.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-Thin.ttf" "%target_font_dir%"
-xcopy /Y "%intermediate_dir%\Montserrat-ThinItalic.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-Black.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-BlackItalic.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-Bold.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-BoldItalic.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-ExtraBold.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-ExtraBoldItalic.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-ExtraLight.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-ExtraLightItalic.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-Italic.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-Light.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-LightItalic.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-Medium.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-MediumItalic.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-Regular.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-SemiBold.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-SemiBoldItalic.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-Thin.ttf" "%target_font_dir%"
+xcopy /Y "%intermediate_dir%/Montserrat-ThinItalic.ttf" "%target_font_dir%"
 
 echo INSTALLATION COMPLETE!
 pause
